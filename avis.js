40a7bf38a10d899bb37a880fa3e94cc5a7725a9e
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-08-02 16:57:42 +0200

    Update

--- avis.js
+++ avis.js
@@ -6,12 +6,27 @@ export function ajoutListenersAvis() {
  
     for (let i = 0; i < piecesElements.length; i++) {
  
-     piecesElements[i].addEventListener("click", function (event) {
+     piecesElements[i].addEventListener("click", async function (event) {
         
         // Récupération de l'attribut data-id avec dataset
         const id = event.target.dataset.id;
-        fetch(`http://localhost:8081/pieces/${id}/avis`);
- 
+        // Stocker la reponse de l'API
+        const reponse = await fetch(`http://localhost:8081/pieces/${id}/avis`);
+        // Reconstituer les données en memoire grace à la méthode json()
+        const avis = await reponse.json();
+        // Ajouter au DOM
+        const pieceElement = event.target.parentElement
+
+        // Creer la balise p
+        const avisElement  = document.createElement("p")
+
+        // Remplir avec le nom de l'utilisateur
+        for(let i = 0; i < avis.length; i++) {
+            // += pour ajouter a la la string a la balise p "avisElement"
+            avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+        }
+        // Ajout au parent
+        pieceElement.appendChild(avisElement);
      });
  
     }
